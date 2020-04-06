924ad928bc285c74d492f2c146e4bf205a2667a5
Tatsuya Terao <tt@connectfree.co.jp>
2020-04-06 14:02:32 +0900

    modify

--- graph/rerooting_dp.cpp
+++ graph/rerooting_dp.cpp
@@ -1,15 +1,15 @@
 //rerooting library by otera
-template< typename Date, typename T>
+template< typename U, typename T>
 struct Rerooting {
 
     struct Edge {
         int to;
-        Date date;
+        U date;
         T dp;
     };
 
     using F1 = function< T(T, T) >;
-    using F2 = function< T(T, Date) >;
+    using F2 = function< T(T, U) >;
 
     vector<vector<Edge>> g;
     vector<T> dp, subdp;
@@ -19,12 +19,12 @@ struct Rerooting {
 
     Rerooting(int n, const F1 &f1, const F2 &f2, const T &ident) : g(n), f1(f1), f2(f2), dp(n, ident), subdp(n, ident) {}
 
-    void add_edge(int s, int t, const Date &d) {
+    void add_edge(int s, int t, const U &d) {
         g[s].emplace_back((Edge) {t, d, ident});
         g[t].emplace_back((Edge) {s, d, ident});
     }
 
-    void add_edge_bi(int s, int t, const Date &d, const Date &e) {
+    void add_edge_bi(int s, int t, const U &d, const U &e) {
         g[s].emplace_back((Edge) {t, d, ident, ident});
         g[t].emplace_back((Edge) {s, e, ident, ident});
     }
